78fb4881f065f79bc154b89b0ef0c8942dbb75e4
Mohan Labhsetwar <mlabhsetwar@madisonlogic.com>
2025-07-26 14:19:20 +0530

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignOut from './pages/SignOut';
+import About from './pages/About';
+import Profile from './pages/Profile';
 const App = () => {
   return (
-    <div className='text-red-500'>
-      App 
-    </div>
+    <BrowserRouter >
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/sign-in' element={<SignIn/>}></Route>
+        <Route path='/sign-out' element={<SignOut/>}></Route>
+        <Route path='/about' element={<About/>}></Route>
+        <Route path='/profile' element={<Profile/>}></Route>
+      </Routes>   
+    </BrowserRouter>
   )
 }
 
